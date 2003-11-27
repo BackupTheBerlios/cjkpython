326812dac553ebc6e48bce9175d4607e74e9613c
perky <perky>
2003-11-27 19:11:02 +0000

    - Update URL - Add comment on --disable-utf

--- cjkcodecs/setup.py
+++ cjkcodecs/setup.py
@@ -27,7 +27,7 @@
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
-# $Id: setup.py,v 1.6 2003/11/27 15:32:00 perky Exp $
+# $Id: setup.py,v 1.7 2003/11/27 19:11:02 perky Exp $
 #
 
 import os, shutil
@@ -125,7 +125,7 @@ setup (name = "cjkcodecs",
        description = "Python Unicode Codecs Collection for CJK Encodings",
        author = "Hye-Shik Chang",
        author_email = "perky@FreeBSD.org",
-       url = "http://sourceforge.net/projects/koco",
+       url = "http://cjkpython.i18n.org/#CJKCodecs",
        license = "BSD Style (without advertisement clause)",
        cmdclass = {'install': Install},
        packages = ['cjkcodecs'],
